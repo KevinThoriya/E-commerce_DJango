a7867c0c1669340129cd29b6ed03ea36eb531989
KevinThoriya <kevinthoriya0074@gmail.com>
2019-12-05 14:25:01 +0530

    adding a shop forntPage and some item with media

--- shop/models.py
+++ shop/models.py
@@ -5,6 +5,13 @@ from django.db import models
 class Product(models.Model):
     product_id = models.AutoField
     product_name = models.CharField( max_length =  50)
-    product_disc = models.CharField( max_length = 300)
+    category = models.CharField(max_length = 50 , default = '')
+    sub_category = models.CharField(max_length = 50 , default = '')
+    prize = models.IntegerField(default=0)
     pub_date = models.DateField()
+    product_disc = models.CharField( max_length = 300)
+    product_image = models.ImageField(default='', upload_to="shop/image")
+
+    def __str__(self) :
+        return self.product_name
 
