cf0042f579c66ac7703a14924a1ec6f3edf4179e
KevinThoriya <kevinthoriya0074@gmail.com>
2019-12-06 12:00:49 +0530

    fetching data and images from daatabase sqlite3

--- shop/views.py
+++ shop/views.py
@@ -1,16 +1,26 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Product
+from math import ceil
 # Create your views here.
+
 def index(request):
-    return render(request,'shop/index.html',)
+    product = Product.objects.all()
+    n = len(product)
+    n_side = n//4 + ceil(n/4 - n//4)
+    print('**********'+str(range(1,n_side)))
+    params = {'product':product, 'n_side':n_side, 'range' : range(1,n_side)}
+    return render(request,'shop/index.html', params)
 
 def contact(request):
     return HttpResponse("contact us")
 
 def about(request):
-    return render(request,'shop/about.html',)
+    params = []
+    return render(request,'shop/about.html',params)
 
 def ProductView(request):
+    params=[]
     return HttpResponse("product view")
 
 def tracker(request):
